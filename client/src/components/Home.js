b220109102d37e8eb11e22b3bd064e6618973089
Chris Watkin <chriswatkin@gmail.com>
2018-05-12 21:46:14 -0400

    initial commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+class Home extends Component {
+  render() {
+    return (
+      <Header as='h1' inverted textAlign='center'>Welcome to DB Camps Finder</Header>
+    );
+  }
+}
+
+export default Home;
